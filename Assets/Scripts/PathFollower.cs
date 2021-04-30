814b170bb47b21d87e5237fc6e96c5fa48090e72
Nathan Campos <eeepc904@gmail.com>
2021-04-30 14:49:14 +0100

    Organized the code of the PathFollower script

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -13,12 +13,41 @@ public class PathFollower : MonoBehaviour {
 	private List<Transform> targets;
 
 	// Start is called before the first frame update
-	void Start() {
+	private void Start() {
+		// Initialize some properties.
 		targets = new List<Transform>();
 		tCharacter = controlledCharacter.transform;
 
+		// Set ourselves as the waypoint container.
+		SetWaypointContainer(this.gameObject);
+	}
+
+	// Update is called once per frame
+	private void Update() {
+		// Check if we have reached our final destination.
+		if (IsAtFinalDestination())
+			return;
+
+		// Move to the current waypoint.
+		MoveTowardsWaypoint();
+
+		// Check if we have reached our waypoint.
+		if (IsAtWaypoint()) {
+			if (Debug.isDebugBuild)
+				Debug.Log("Arrived at: " + targets[0].gameObject.name);
+
+			// Move to the next waypoint.
+			MoveToNextWaypoint();
+		}
+	}
+
+	/// <summary>
+	/// Sets the current waypoints container <see cref="GameObject"/>
+	/// </summary>
+	/// <param name="waypointsParent">Container of waypoints.</param>
+	public void SetWaypointContainer(GameObject waypointsParent) {
 		// Go through GameObject childs.
-		foreach (Transform transform in gameObject.GetComponentsInChildren<Transform>()) {
+		foreach (Transform transform in waypointsParent.GetComponentsInChildren<Transform>()) {
 			// Ignore if we got ourself.
 			if (transform.gameObject == this.gameObject)
 				continue;
@@ -28,23 +57,36 @@ public class PathFollower : MonoBehaviour {
 		}
 	}
 
-	// Update is called once per frame
-	void Update() {
-		// Check if we have reached our final destionation.
-		if (targets.Count == 0)
-			return;
-
+	/// <summary>
+	/// Moves the character towards the current waypoint.
+	/// </summary>
+	private void MoveTowardsWaypoint() {
 		// Move our object towards the waypoint.
 		float step = movementSpeed * Time.deltaTime;
 		tCharacter.position = Vector3.MoveTowards(tCharacter.position, targets[0].position, step);
+	}
 
-		// Check if we have reached our waypoint.
-		if (Vector3.Distance(tCharacter.position, targets[0].position) < 0.001f) {
-			if (Debug.isDebugBuild)
-				Debug.Log("Arrived at: " + targets[0].gameObject.name);
-			
-			// Remove the first element to go to the next one.
-			targets.RemoveAt(0);
-		}
+	/// <summary>
+	/// Starts moving the character to its next waypoint.
+	/// </summary>
+	public void MoveToNextWaypoint() {
+		// Remove the first element to go to the next one.
+		targets.RemoveAt(0);
+	}
+
+	/// <summary>
+	/// Checks if the target character is at the current waypoint.
+	/// </summary>
+	/// <returns><code>True</code> if the character is at the current waypoint.</returns>
+	public bool IsAtWaypoint() {
+		return Vector3.Distance(tCharacter.position, targets[0].position) < 0.001f;
+	}
+
+	/// <summary>
+	/// Checks if the target character is at its final destination.
+	/// </summary>
+	/// <returns><code>True</code> if the character is at its final destination.</returns>
+	public bool IsAtFinalDestination() {
+		return targets.Count == 0;
 	}
 }
