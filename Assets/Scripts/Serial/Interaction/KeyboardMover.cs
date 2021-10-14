cdf1ed3da86840720bd4da2acb230d05caeeb584
Nathan Campos <eeepc904@gmail.com>
2021-10-14 22:26:13 +0100

    Stuff that was changed for the recording last semester

--- Assets/Scripts/Serial/Interaction/KeyboardMover.cs
+++ Assets/Scripts/Serial/Interaction/KeyboardMover.cs
@@ -9,19 +9,19 @@ using UnityEngine;
 /// <inheritdoc/>
 public class KeyboardMover : IMover {
 	public float MoveBackward() {
-		return Input.GetKey(KeyCode.DownArrow) ? 1.0f : 0.0f;
-	}
-
-	public float MoveForward() {
 		return Input.GetKey(KeyCode.UpArrow) ? 1.0f : 0.0f;
 	}
 
+	public float MoveForward() {
+		return Input.GetKey(KeyCode.DownArrow) ? 1.0f : 0.0f;
+	}
+
 	public float MoveLeft() {
-		return Input.GetKey(KeyCode.LeftArrow) ? 1.0f : 0.0f;
+		return Input.GetKey(KeyCode.RightArrow) ? 1.0f : 0.0f;
 	}
 
 	public float MoveRight() {
-		return Input.GetKey(KeyCode.RightArrow) ? 1.0f : 0.0f;
+		return Input.GetKey(KeyCode.LeftArrow) ? 1.0f : 0.0f;
 	}
 
 	public void OnQuit() {
